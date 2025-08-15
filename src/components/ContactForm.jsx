56da788cc91ed82394e5b3aa5eaa7ebabf44de25
caetano borba peruzzo <CBperuzzo4@gmail.com>
2025-08-15 18:03:56 -0300

    contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,24 +1,44 @@
+import toast, { Toaster } from "react-hot-toast";
+import { useRef } from "react";
+
 export function ContactForm(props){
+
+    const nameRef = useRef()
+    const emailRef = useRef()
+    const messageRef = useRef()
+
+    function submit(e){
+        e.preventDefault();
+        setTimeout( () => {
+
+            toast.success("Message sent!");
+            nameRef.current.value = "";
+            emailRef.current.value = "";
+            messageRef.current.value = "";
+
+        }, 350)
+    }
+
     return(
         <section ref={props.contactRef} className="contact-form-section">
-            <form className="contact-form big-card">
+            <form onSubmit={submit} className="contact-form big-card">
                 <h1 className="contact-me-title medium-title-font">
                     {props.title}
                 </h1>
                 <ul className="inputs-list ">
                     <li>
                         <label className="subtitle" htmlFor="name">{props.name}</label>
-                        <input className="input-box dark-text" id="name" type="text"></input>
+                        <input ref={nameRef} className="input-box dark-text" id="name" type="text"></input>
                     </li>
 
                     <li>
                         <label className="subtitle" htmlFor="email">{props.email}</label>
-                        <input className="input-box dark-text" id="email" type="emial"></input>
+                        <input ref={emailRef} className="input-box dark-text" id="email" type="emial"></input>
                     </li>
 
                     <li>
                         <label className="subtitle" htmlFor="message">{props.message}</label>
-                        <textarea className="input-box dark-text" id="message" rows={props.rowCount}>
+                        <textarea ref={messageRef} className="input-box dark-text" id="message" rows={props.rowCount}>
 
                         </textarea>
                     </li>
@@ -27,6 +47,7 @@ export function ContactForm(props){
                 <button id="submit-contact" className="submit-contact-button fat-medium-text-font" type="submit">
                     {props.send}
                 </button>
+                <Toaster/>
             </form>
         </section>
     )
